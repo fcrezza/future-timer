3610fb94a745d69cdc1f72bbe0c3df824ebc910e
Anang Fachreza <fachrezanang@gmail.com>
2019-10-08 23:41:35 +0700

    add list input component to the folder

---
+++ src/view/ListInput.js
@@ -0,0 +1,30 @@
+import React from "react";
+import TimeInput from "./TimeInput";
+import { Wrapper } from '../style/ListInput';
+
+export default function ListInput({ timer, handleSetTimer }) {
+	const {hour, minute, second} = timer
+
+	return (
+		<Wrapper>
+			<TimeInput
+				id="hour"
+				text="Jam"
+				time={hour}
+				handleSetTimer={handleSetTimer}
+			/>
+			<TimeInput
+				id="minute"
+				text="Menit"
+				time={minute}
+				handleSetTimer={handleSetTimer}
+			/>
+			<TimeInput
+				id="second"
+				text="Detik"
+				time={second}
+				handleSetTimer={handleSetTimer}
+			/>
+		</Wrapper>
+	);
+}
