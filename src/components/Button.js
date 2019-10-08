a65fee66974bad1f88f525ca06536703cca44b10
Anang Fachreza <fachrezanang@gmail.com>
2019-10-08 23:42:28 +0700

    split style and add some logic

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,24 +1,24 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import { isEmpty } from '../logic/isEmpty';
+import {StyledButton} from '../style/Button';
 
-const StyledButton = styled.a`
-	text-decoration: none;
-	color: #fff;
-	font-weight: 600;
-	display: block;
-	width: ${props => props.width ? "40%" : "100%"};
-	border-radius: 5px;
-	background-color: ${props => props.secondary ? "#e02272" : "#2100ec"};
-	padding: 10px;
-	margin-top: 20px;
-	text-align: center;
-	outline: none;
-	cursor: pointer;
-	margin-bottom: 30px;
-`
+export default function Button({
+	text,
+	handleSubmit,
+	width = false,
+	secondary = false,
+	value
+}) {
+	if (isEmpty(value)) {
+		return <StyledButton disabled>{text}</StyledButton>;
+	}
 
-const Button = ({text, handleSubmit, width=false, secondary=false}) => {
-	return <StyledButton onClick={handleSubmit} width={width ? 1 : 0} secondary={secondary ? 1 : 0}>{text}</StyledButton>
-}
-
-export default Button
+	return (
+		<StyledButton
+			onClick={handleSubmit}
+			width={width ? 1 : 0}
+			secondary={secondary ? 1 : 0}>
+			{text}
+		</StyledButton>
+	);
+};
