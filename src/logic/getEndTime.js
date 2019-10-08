c21af54aa6528aa1ee4ccbfe17e64ddf0b91f92e
Anang Fachreza <fachrezanang@gmail.com>
2019-10-08 23:39:58 +0700

    make logic folder

---
+++ src/logic/getEndTime.js
@@ -0,0 +1,18 @@
+export function getEndTime (hour, minute, second) {
+	const currentYear = new Date().getFullYear();
+	const currentMonth = new Date().getMonth();
+	const currentDay = new Date().getDate();
+	const currentHour = new Date().getHours() + Number(hour);
+	const currentMinute = new Date().getMinutes() + Number(minute);
+	const currentSecond = new Date().getSeconds() + Number(second);
+	var end = new Date(
+		currentYear,
+		currentMonth,
+		currentDay,
+		currentHour,
+		currentMinute,
+		currentSecond
+	).getTime();
+
+	return end;
+}
